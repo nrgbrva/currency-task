9f69a72103dd5109c96c4164acf5cfb910e1c61d
nrgbrva <hanbarovan@yandex.az>
2022-11-12 12:10:38 +0400

    almost finished

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 ///renglerin ve valyutanin secilmesi
 let firstvalue;
 let secondvalue;
+let firstAmount;
 let unitStart = document.querySelectorAll('.primary .box');
 unitStart.forEach(element => {
     element.addEventListener('click', function () {
@@ -8,6 +9,7 @@ unitStart.forEach(element => {
         element.classList.toggle("first");
         firstvalue = document.querySelector('.primary>.first')
         firstvalue = firstvalue.innerText
+        firstAmount = document.querySelector('.starting h2')
     }
     )
 });
@@ -19,15 +21,20 @@ unitEnd.forEach(element => {
         element.classList.toggle("first");
         secondvalue = document.querySelector('.secondary>.first')
         secondvalue = secondvalue.innerText
-        let lastAmount=document.querySelector('.ending h2')
+        let lastAmount = document.querySelector('.ending h2')
         fetch(`https://api.exchangerate.host/latest?base=${firstvalue}&symbols=${secondvalue} `)
-            .then(x => x.json()).then(function(x){
-                 rating=x.rates
-                 rating=rating[`${secondvalue}`]
-                console.log(x)
-                console.log(rating[`${secondvalue}`])
-                console.log(typeof rating)
-                lastAmount.innerHTML=rating
+            .then(x => x.json()).then(function (x) {
+             
+                console.log(Object.values(x.rates)[0])
+                
+                rating = Object.values(x.rates)[0]
+
+                console.log(rating);
+                firstAmount = +firstAmount.innerText;
+
+                let sum = rating * firstAmount;
+                console.log(sum)
+                lastAmount.textContent=sum
 
             })
     })
