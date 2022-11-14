5aae13e455a22d063497fe4b5f72084f88afb939
nrgbrva <hanbarovan@yandex.az>
2022-11-14 11:28:19 +0400

    second input

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let rating;
 let info2 = document.querySelector('#news2');
 let info1 = document.querySelector('#news1');
 //// select unit
+firstAmount = document.querySelector('.starting input');
+lastAmount=document.querySelector('.ending input');
 let unitStart = document.querySelectorAll('.primary .box');
 unitStart.forEach(element => {
     element.addEventListener('click', function () {
@@ -14,7 +16,9 @@ unitStart.forEach(element => {
         element.classList.toggle("first");
         firstvalue = document.querySelector('.primary>.first')
         firstvalue = firstvalue.innerText
+        
         editInfo();
+        
     }
     )
 });
@@ -26,6 +30,7 @@ unitEnd.forEach(element => {
         secondvalue = document.querySelector('.secondary>.first')
         secondvalue = secondvalue.innerText
         editInfo()
+        
     })
 
 });
@@ -33,8 +38,10 @@ unitEnd.forEach(element => {
 /// edit info box
 function editInfo(){
     getInfo()
+    valid();
     info2.innerText = `1 ${secondvalue} = ${rating}${firstvalue}`
     info1.innerText = `1 ${firstvalue} = ${rating}${secondvalue}`
+    
 }
 ///////
 ///// get rate info
@@ -46,24 +53,31 @@ function getInfo(){
 }
 /////////
 /// edit boxes according to given value
-firstAmount = document.querySelector('.starting input');
-lastAmount=document.querySelector('.ending input');
-firstAmount.addEventListener('keyup',function(){
-    let val1=firstAmount.value;
-    let val2=val1*rating
-    lastAmount.value=val2
-    validation()
-   
-})
-lastAmount.addEventListener('keyup',function(){
-    let val1=lastAmount.value;
-    let val2=val1*rating
-    firstAmount.value=val2
-    validation()
-})
 
-function validation(){
-    if( document.querySelectorAll('.first').length==0){
-        alert('siz valyuta secimi etmediniz')
-    }
+
+function first(){
+    firstAmount.addEventListener('keyup',function(){
+        let val1=firstAmount.value;
+        let val2=val1*rating
+        lastAmount.value=val2
+       
+    })
+}
+function last(){
+    
+    lastAmount.addEventListener('keyup',function(){
+        fetch(`https://api.exchangerate.host/latest?base=${secondvalue}&symbols=${firstvalue} `)
+        .then(x => x.json()).then(function (x) {
+            rating = Object.values(x.rates)[0]
+        })
+        let val1=lastAmount.value;
+        let val2=val1*rating
+        firstAmount.value=val2
+    })
+    
+}
+first();
+last();
+function valid(){
+    if(document.querySelectorAll('.first').length<2){alert('valyuta vahidi secin')}
 }
